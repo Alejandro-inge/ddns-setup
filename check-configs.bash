3d25a35d23bbaeb1d2872100e595f0e07a2309e7
night <agexandro@gmail.com>
2026-04-02 23:43:44 -0600

    Removing unnecesary code

--- check-configs.bash
+++ check-configs.bash
@@ -1,14 +1,10 @@
 #!/bin/bash
 cd "$(dirname "$0")"
 
-# Check dhcpd configuration file
-dhcpd -t -cf ./dhcp/dhcpd.conf
-
 #check zone configuration files
 named-checkzone local.lan ./dns/zones/net.local.lan
 named-checkzone 4.0.10.in-addr.arpa ./dns/zones/net.local.lan.inverse
 
 # Check bind configuration files
-named-checkconf ./dns/named.conf
 named-checkconf ./dns/named.conf.local
 named-checkconf ./dns/named.conf.options
